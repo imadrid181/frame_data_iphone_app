28946d6690cfa7e36b60f3c80e1e2cfe4aa649e1
Ivan Madrid <imadrid181@gmail.com>
2024-03-03 13:46:59 -0600

    Added function to retrieve all datatables for a character and merge them The function retrieves all the frame data information and merges them into one dataframe. The next goal is to change that dataframe into json format and store it in a database.

--- web_scraper.py
+++ web_scraper.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
 
+DATA_TABLES = ["DataTables_Table_0", "DataTables_Table_1", "DataTables_Table_2", "DataTables_Table_3", "DataTables_Table_4"]
+
 """Loads Dynamic portion of website and then return the source code."""
 def parse(url):
     options = Options()
@@ -43,34 +45,21 @@ def flatten_concatenation(array):
         flat_arr += row
     return flat_arr
 
+def getFrameData(character):
+    url = "https://www.dustloop.com/w/GBVSR/{character}/Frame_Data".format(character=character)
+    sourceCode = parse(url)
 
-url = "https://www.dustloop.com/w/GBVSR/Eustace/Frame_Data"
-sourceCode = parse(url)
-
-table = getTable(sourceCode, "DataTables_Table_0")
-df0 = tableToDataFrame(table)
-print(df0)
-
-print()
-
-table = getTable(sourceCode, "DataTables_Table_1")
-df1 = tableToDataFrame(table)
-print(df1)
-
-print()
-
-table = getTable(sourceCode, "DataTables_Table_2")
-df2 = tableToDataFrame(table)
-print(df2)
-
-print()
-
-table = getTable(sourceCode, "DataTables_Table_3")
-df3 = tableToDataFrame(table)
-print(df3)
-
-print()
-
-table = getTable(sourceCode, "DataTables_Table_4")
-df4 = tableToDataFrame(table)
-print(df4)
+    frameData_DF = None
+    for table in DATA_TABLES:
+        if frameData_DF is None:
+            tempTable = getTable(sourceCode, table)
+            frameData_DF = tableToDataFrame(tempTable)
+        else:
+            tempTable = getTable(sourceCode, table)
+            tempDF = tableToDataFrame(tempTable)
+            frameData_DF = pd.concat([frameData_DF, tempDF], axis=0)
+        
+    return frameData_DF
+        
+fr = getFrameData('Eustace')
+print(pd.DataFrame.to_json(fr))
